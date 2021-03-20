e6a9219ec8aeac18efdf55b765895c51f91a9a88
Simon Elvery <simon@elvery.net>
2021-03-21 07:40:31 +1000

    add test for rest/spread routes

---
+++ src/routes/[...spread].svelte
@@ -0,0 +1,18 @@
+<script context="module">
+  export async function load({
+    page: {
+      params: { spread },
+    },
+  }) {
+    console.log('spread :>> ', spread);
+    return { props: { spread } };
+  }
+</script>
+
+<script>
+  import { page } from '$app/stores';
+  export let spread;
+</script>
+
+<p data-source="load">{spread}</p>
+<p data-source="store">{$page.params.spread}</p>
